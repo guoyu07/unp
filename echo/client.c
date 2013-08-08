ebe6399df1e1f313e63b290acc797ad169b08657
potter <potter@server.dikeen.com>
2013-08-08 11:21:31 +0800

    client use non block

--- echo/client.c
+++ echo/client.c
@@ -7,9 +7,18 @@
 #include <sys/socket.h>
 #include <sys/select.h>
 #include <math.h>
+#include <fcntl.h>
+#include <errno.h>
 #include "echo.h"
 
 void str_cli(FILE *, int);
+
+void set_nonblock(int fd) {
+    int val;
+
+    val = fcntl(fd, F_GETFL, 0);
+    fcntl(fd, F_SETFL, val | O_NONBLOCK);
+}
 
 int main(int argc, char *argv[]) {
     int sockfd;
@@ -35,34 +44,106 @@ int main(int argc, char *argv[]) {
     exit(0);
 }
 
-void str_cli(FILE *fp, int fd) {
+void str_cli(FILE *fp, int sockfd) {
     int maxfdp1;
-    fd_set rset;
-    char sendline[MAXLINE], recvline[MAXLINE];
+    ssize_t n, nwritten;
+    fd_set rset, wset;
+    char to[MAXLINE], fr[MAXLINE];
+    char *toiptr, *tooptr, *friptr, *froptr;
 
-    FD_ZERO(&rset);
+    toiptr = tooptr = to;
+    friptr = froptr = fr;
+
+    set_nonblock(sockfd);
+    set_nonblock(STDIN_FILENO);
+
+    maxfdp1 = max(STDIN_FILENO, sockfd) + 1;
+
     for( ; ; ) {
-	FD_SET(fileno(fp), &rset);
-	FD_SET(fd, &rset);
-	maxfdp1 = max(fileno(fp), fd) + 1;
-	select(maxfdp1, &rset, NULL, NULL, NULL);
+	FD_ZERO(&rset);
+	FD_ZERO(&wset);
 
-	if(FD_ISSET(fd, &rset)) {
-	    if(read(fd, recvline, MAXLINE) == 0) {
-		printf("str_cli: server terminated prematurely\n");
+	/*if to buf had some space yet, then*/
+	if(toiptr < &to[MAXLINE]) {
+	    FD_SET(STDIN_FILENO, &rset);
+	}
+	if(friptr < &fr[MAXLINE]) {
+	    FD_SET(sockfd, &rset);
+	}
+
+	/*如果缓冲区中有数据未发送完，需要注册写事件*/
+	if(tooptr != toiptr) {
+	    FD_SET(sockfd, &wset);
+	}
+	if(froptr != friptr) {
+	    FD_SET(STDOUT_FILENO, &wset);
+	}
+
+	select(maxfdp1, &rset, &wset, NULL, NULL);
+
+	if(FD_ISSET(sockfd, &rset)) {
+	    if((n = read(sockfd, friptr, &fr[MAXLINE] - friptr)) < 0) {
+		if(errno != EWOULDBLOCK) {
+		    fprintf(stderr, "str_cli: read sock error\n");
+		    exit(1);
+		}
+	    } else if(n == 0) {
+		/*EOF on socket*/
+		fprintf(stderr, "str_cli: server terminated prematurely\n");
 		exit(1);
+	    } else {
+		fprintf(stderr, "read %d byte from socket\n", n);
+		friptr += n;
+
+		/*listen stdout write*/
+		FD_SET(STDOUT_FILENO, &wset);
 	    }
-	    fputs(recvline, stdout);
 	}
 
-	if(FD_ISSET(fileno(fp), &rset)) {
-	    if(fgets(sendline, MAXLINE, fp) == NULL) {
-		printf("fgets return NULL\n");
-		return;
+	if(FD_ISSET(STDIN_FILENO, &rset)) {
+	    if((n = read(STDIN_FILENO, toiptr, &to[MAXLINE] - toiptr)) < 0) {
+		if(errno != EWOULDBLOCK) {
+		    fprintf(stderr, "read stdin error\n");
+		    exit(1);
+		}
+	    } else if(n == 0) {
+		/* EOF on stdin
+		 * if to buf is empty,then close socket write endian
+		 * send FIN
+		 */
+		if(tooptr == toiptr) {
+		    shutdown(sockfd, SHUT_WR);
+		}
+	    } else {
+		toiptr += n;
+		FD_SET(sockfd, &wset);
 	    }
-	    printf("write something\n");
-	    write(fd, sendline, strlen(sendline));
-	    printf("w\n");
 	}
+
+	if(FD_ISSET(STDOUT_FILENO, &wset) && ((n = friptr - froptr) > 0)) {
+	    if((nwritten = write(STDOUT_FILENO, froptr, n)) < 0) {
+		if(errno != EWOULDBLOCK) {
+		    fprintf(stderr, "write stdout error\n");
+		    exit(1);
+		}
+	    } else {
+		froptr += nwritten;
+		if(froptr == friptr) froptr = friptr = fr;
+	    }
+	}
+
+	if(FD_ISSET(sockfd, &wset) && ((n = toiptr - tooptr) > 0)) {
+	    if((nwritten = write(sockfd, tooptr, n)) < 0) {
+		if(errno != EWOULDBLOCK) {
+		    fprintf(stderr, "write sockfd error\n");
+		    exit(1);
+		}
+	    } else {
+		tooptr += nwritten;
+		if(tooptr == toiptr) tooptr = toiptr = to;
+	    }
+	
+	}
+
     }
 }
